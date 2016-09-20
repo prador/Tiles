dc614cfa615e7391bff088300d23f5cb088f5703
pradyumna surampudi <pradyumna.surampudi@gmail.com>
2016-09-21 01:57:37 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 // using css files and jquery. files can be local or URL based writted in css.
 
-  var index = 0;
-  var hovEnter;
-  var hovLeave;
-  $( ".tiles" )
-  .mouseenter(function() {
-    $(".tile-wrapper").removeClass(hovLeave,1500,"easeInOut");
-    index = $(this).attr("index");
-    hovEnter = "hover-"+index;
-    hovLeave = hovEnter;
-    $(".tile-wrapper").addClass(hovEnter,1500,"easeInOut");
-  });
+  // var index = 0;
+  // var hovEnter;
+  // var hovLeave;
+  // $( ".tiles" )
+  // .mouseenter(function() {
+  //   $(".tile-wrapper").removeClass(hovLeave,1500,"easeInOut");
+  //   index = $(this).attr("index");
+  //   hovEnter = "hover-"+index;
+  //   hovLeave = hovEnter;
+  //   $(".tile-wrapper").addClass(hovEnter,1500,"easeInOut");
+  // });
 
 // array of image URLs
   var imgArr = ['http://bit.ly/2cCQ2VK','http://bit.ly/2decFOU','http://bit.ly/2d2oO6c','http://bit.ly/2cCPdMK',
@@ -19,22 +19,22 @@
 
 // jquery only example
 
-// $(".tiles")
-//   .mouseenter(function() {
-//     index = $(this).attr("index");
-//     var image = 'https://github.com/prador/Tiles/tree/master/images/'+index+'.jpg';
-//     // use local files for images
-//     $(".tile-wrapper").css('background-image','url('+image+')');
-//     // Use cdn based from array
-//     // $(".tile-wrapper").css('background-image','url('+imgArr[--index]+')');
-//   });
+$(".tiles")
+  .mouseenter(function() {
+    index = $(this).attr("index");
+    var image = '../images/'+index+'.jpg';
+    // use local files for images
+    $(".tile-wrapper").css('background-image','url('+image+')');
+    // Use cdn based from array
+    // $(".tile-wrapper").css('background-image','url('+imgArr[--index]+')');
+  });
 
 
 // javascript only example
 
 // function imgChange(index){
 // use local files for images
-//     var image = 'https://github.com/prador/Tiles/tree/master/images/'+index+'.jpg';
+//     var image = '../images/'+index+'.jpg';
 //     wrap.style.backgroundImage = "url("+image+")";
 // Use cdn based from array
 //     wrap.style.backgroundImage = "url("+imgArr[--index]+")";
